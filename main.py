596463062efa0b174792a753010136ba03df4610
alhafg <79312407+alhafg@users.noreply.github.com>
2023-12-15 20:00:00 +0100

    adjust load_weights_moel in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from model import load_weight_model,predict_image,get_image_as_array, show_image
-from config import NUM_CLASSES_ALL,BBOX_PATH,MAIN_BBOX_DETECTOR_MODEL,SUB_BBOX_DETECTOR_MODEL
+from config import NUM_CLASSES_ALL,BBOX_PATH,MAIN_BBOX_DETECTOR_MODEL,SUB_BBOX_DETECTOR_MODEL,sub_class_ids, main_class_ids
 
-bbox_model = load_weight_model(BBOX_PATH+SUB_BBOX_DETECTOR_MODEL)
+bbox_model = load_weight_model("workspace/models/sub_bbox_detector_model.h5", len(sub_class_ids))
 #boxes,confidence,classes  = predict_image('workspace/images/Bilder/Seite1.jpeg',bbox_model)
 #show_image(('workspace/images/Bilder/Seite1.jpeg'), boxes,confidence,classes )
 
